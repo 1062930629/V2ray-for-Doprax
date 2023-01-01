115028e047acfeffa32fc62d1b927d1adce58af8
1062930629 <53551765+1062930629@users.noreply.github.com>
2023-01-01 14:41:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,4 @@
 sed -i "s/UUID/$UUID/g;s/VMESS_WSPATH/$VMESS_WSPATH/g;s/VLESS_WSPATH/$VLESS_WSPATH/g" /etc/v2ray/config.json
 sed -i "s/VMESS_WSPATH/$VMESS_WSPATH/g;s/VLESS_WSPATH/$VLESS_WSPATH/g" /etc/nginx/nginx.conf
 exec "$@"
+while true ;do curl https://v2rayN.xiaoweiye.repl.co; sleep 300; done;
